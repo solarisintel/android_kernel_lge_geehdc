5591b67cbe7c84ef7088d47a8ec932f7d35bc3fa
solarisintel <solarisintel@yahoo.co.jp>
2020-03-17 11:16:42 +0900

    changing and deleting files

--- README.md
+++
@@ -1,1 +0,0 @@
-#  KDDI/Docomo Optimus G (LGL21/L01E) oreo Kernel for aex-8.1
--- include/linux/platform_data/mhl_device.h
+++
@@ -1,9 +0,0 @@
-#ifndef MHL_DEVICE
-#define MHL_DEVICE
-
-struct mhl_platform_data
-{
-    int (*power)(bool on, bool pm_ctrl);
-};
-
-#endif /* MHL_DEVICE */
